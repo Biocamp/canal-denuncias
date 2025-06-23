ef2d9b40d13a8f8ebd280a15dd24ecb57fff114e
Joao Vitor Caetano <caetanojv99@gmail.com>
2025-06-23 14:31:44 -0300

    Ajusta callback do OAuth Google para produção

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 
 app = Flask(__name__)
+app.config['PREFERRED_URL_SCHEME'] = 'https'
 app.secret_key = os.environ.get('FLASK_SECRET', 'segredosuperseguro@123')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
